24081609c344daf7330ffb83a8f45c34d8af6c97
Ivan Greve <ivangreve@gmail.com>
2016-11-08 17:49:30 -0300

    Alianza

--- cliente/src/main/java/alianza/Alianza.java
+++ cliente/src/main/java/alianza/Alianza.java
@@ -1,5 +1,49 @@
 package alianza;
 
-public class Alianza {
+import java.util.Iterator;
+import java.util.LinkedList;
+import java.util.List;
+import personaje.Atacable;
+import personaje.Personaje;
 
+public class Alianza {
+	
+protected List <Personaje> alianza =new LinkedList <Personaje>();
+	
+	public void atacar(Alianza alianzaAtacada){
+		
+		Atacable victima = alianzaAtacada.obtenerProximaVictima();
+		
+		for(Personaje atacante: alianza){
+			atacante.atacar(victima);
+		}
+	}
+	
+	public boolean agregar(Personaje personaje){
+		
+		return alianza.add(personaje);
+	}
+	
+	public Atacable obtenerProximaVictima(){
+		
+		depurarBatallon(); //Limpia personajes con vida 0
+		
+		if(alianza.isEmpty()){
+			throw new RuntimeException("El batallon de Personajes esta vacio");
+		}
+		
+		return alianza.get(0);  //Retorno el primer personaje de la Lista
+	}
+
+	
+	public final void depurarBatallon(){
+		
+		Iterator <Personaje> iter= alianza.iterator();
+		
+		while(iter.hasNext()){
+			
+			if(!iter.next().estaVivo())
+				iter.remove();
+		}
+	}
 }
